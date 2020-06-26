2a37fb85e6a97ba86d5117e48de3067bbf8da6eb
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-06-27 00:32:09 +0530

    added api

--- Githubfunc/settings.py
+++ Githubfunc/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gitapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Githubfunc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
--- gitapp/views.py
+++ gitapp/views.py
@@ -1,3 +1,14 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home(request):
+    if request.method=='GET':
+        username=request.GET['username']
+        url='https://api.github.com/users/%s' %username
+        response=requests.get(url)
+        user=response.json()
+        #idofuser=user.id
+        #profilepic='https://avatars3.githubusercontent.com/u/%s?v=4' %idofuser
+    return render(request,'home.html',{'user':user})
