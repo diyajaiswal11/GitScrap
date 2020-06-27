b8233ce589000a572b5331236beb522564cd72ed
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-06-27 22:54:04 +0530

    made changes

--- gitapp/views.py
+++ gitapp/views.py
@@ -6,9 +6,13 @@ from django.shortcuts import render
 def home(request):
     if request.method=='GET':
         username=request.GET['username']
-        url='https://api.github.com/users/%s' %username
-        response=requests.get(url)
-        user=response.json()
+        user=requests.get('https://api.github.com/users/%s' % username).json()
+        repos=requests.get('https://api.github.com/users/%s/repos' %username).json()
+        l=[]
+        commit=len(requests.get('https://api.github.com/repos/%s/%s/commits' % (username ,repos[0]["name"])).json())
+        print(commit)
+        
+        #print(repos)
         #idofuser=user.id
         #profilepic='https://avatars3.githubusercontent.com/u/%s?v=4' %idofuser
-    return render(request,'home.html',{'user':user})
+    return render(request,'home.html',{'user':user, 'repos':repos})
