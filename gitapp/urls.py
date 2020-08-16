0f299217122fe697c87ad10f8e6e7375ccbd6be4
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-08-17 00:27:00 +0530

    user profile page

--- gitapp/urls.py
+++ gitapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('profile/',views.detail,name='detail'),
 ]
--- gitapp/views.py
+++ gitapp/views.py
@@ -1,17 +1,20 @@
 import requests
 from django.shortcuts import render
 
-
 def home(request):
+    return render(request,'index.html')
+
+
+def detail(request):
     if request.method=='GET':
-        username=request.GET['username']
-        user=requests.get('https://api.github.com/users/%s' % username).json()
-        repos=requests.get('https://api.github.com/users/%s/repos' %username).json()
-        l=[]
-        commit=len(requests.get('https://api.github.com/repos/%s/%s/commits' % (username ,repos[0]["name"])).json())
-        print(commit)
+       username=request.GET['username']
+       user=requests.get('https://api.github.com/users/%s' % username).json()
+       repos=requests.get('https://api.github.com/users/%s/repos' %username).json()
+       l=[]
+    #    commit=len(requests.get('https://api.github.com/repos/%s/%s/commits' % (username ,repos[0]["name"])).json())
+    #    print(commit)
         
-        #print(repos)
-        #idofuser=user.id
-        #profilepic='https://avatars3.githubusercontent.com/u/%s?v=4' %idofuser
-    return render(request,'home.html',{'user':user, 'repos':repos})
+      #print(repos)
+    #     #idofuser=user.id
+    #     #profilepic='https://avatars3.githubusercontent.com/u/%s?v=4' %idofuser
+    return render(request,'profile.html',{'user':user, 'repos':repos})
