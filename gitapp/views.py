da636a47c2628ef7662be40135048c41db02619f
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-08-18 02:56:37 +0530

    changes

--- gitapp/views.py
+++ gitapp/views.py
@@ -10,11 +10,6 @@ def detail(request):
        username=request.GET['username']
        user=requests.get('https://api.github.com/users/%s' % username).json()
        repos=requests.get('https://api.github.com/users/%s/repos' %username).json()
-       l=[]
-    #    commit=len(requests.get('https://api.github.com/repos/%s/%s/commits' % (username ,repos[0]["name"])).json())
-    #    print(commit)
-        
-      #print(repos)
-    #     #idofuser=user.id
-    #     #profilepic='https://avatars3.githubusercontent.com/u/%s?v=4' %idofuser
+      
+ 
     return render(request,'profile.html',{'user':user, 'repos':repos})
