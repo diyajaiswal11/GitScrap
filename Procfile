9085e30d984c5ba3bb706f89586ee6dcabf03e1c
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2021-01-28 02:48:45 +0530

    minor issues

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Githubfunc.wsgi --log-file -
--- gitenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\welcome\githubfunc\gitenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
